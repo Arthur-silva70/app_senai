1bd3abc254db08b32b6bfeb19f59c96d8597fb5b
Arthur <ag3790817@gmail.com>
2024-06-10 21:41:17 -0300

    Projeto finalizado

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import telaLogin from "./telaLogin";
+import telaGeral from "./telaGeral";
+import telaCadastro from "./telaCadastro";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+    <Stack.Navigator initialRouteName="Tela Login">
+      <Stack.Screen name="Tela Login" component={telaLogin} options={{headerShown:false}}/>
+      <Stack.Screen name="Tela Geral" component={telaGeral} options={{headerShown:false}}/>
+      <Stack.Screen name="Tela Cadastro" component={telaCadastro} options={{headerShown:false}}/>
+    </Stack.Navigator>
+  </NavigationContainer>
+  ); 
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
